d9b3127bbfb03c27a34d3072a51f3cebaade0e29
Lawskyy <115343710+Lawskyy@users.noreply.github.com>
2022-10-08 15:40:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6285876617548','6285876617548','6285876617548','6285876617548']
+global.premium = ['6285876617548']
+global.packname = 'sir.lawskyyy'
 global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.sessionName = 'lawskyy'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
